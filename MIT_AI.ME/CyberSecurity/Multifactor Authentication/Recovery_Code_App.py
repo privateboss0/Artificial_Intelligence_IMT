7d468618f69fa1d7f6f924c5f4201e4dc7918160
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-13 13:15:14 +0100

    HOTP and TOTP 2FA/MFA app with Authenticator Integration

--- MIT_AI.ME/CyberSecurity/Multifactor Authentication/Recovery_Code_App.py
+++ MIT_AI.ME/CyberSecurity/Multifactor Authentication/Recovery_Code_App.py
@@ -3,7 +3,7 @@ import pyotp
 
 secretkey = "3Private6Public7" 
 #secretkey = pyotp.random_base32() Either use a static key or random key, both kept by the service provider.
-#but the program works inversely with the recovery code app set to static, while MFA na d Integration app in random.
+#but the program works inversely with the recovery code app secretkey set to static, while MFA and Integration app secretkey in random.
 
 counter = 0
 
