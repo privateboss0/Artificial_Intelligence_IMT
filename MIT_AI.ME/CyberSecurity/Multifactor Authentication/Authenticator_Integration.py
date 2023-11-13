47cefb3dcb01af024641e1e27f72d1b745f444c6
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-13 09:39:17 +0100

    HOTP and TOTP 2FA/MFA app with Authenticator Integration

--- MIT_AI.ME/CyberSecurity/Multifactor Authentication/Authenticator_Integration.py
+++ MIT_AI.ME/CyberSecurity/Multifactor Authentication/Authenticator_Integration.py
@@ -2,11 +2,11 @@ import time
 import pyotp
 import qrcode
 
-secretkey = "PublicPrivate1679@#"
+secretkey = "PublicPrivate"
 
 Uniform_Resource_Identifier = pyotp.totp.TOTP(secretkey).provisioning_uri(name="Aola", issuer_name= "Privateboss_App")
 
 print(Uniform_Resource_Identifier)
 
-#Make and download the QRcode for Privateboss_App to use with Google Authenticator app
-qrcode.make(Uniform_Resource_Identifier).save("totp.png")
+#Make and download the QRcode for Privateboss_App to use with Google/Microsoft Authenticator app
+qrcode.make(Uniform_Resource_Identifier).save("QRCode.png")
