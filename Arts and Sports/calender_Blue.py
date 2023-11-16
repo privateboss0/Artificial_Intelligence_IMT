649fcd6201f9e3ee107b4c923b71db60479041a0
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-16 23:26:35 +0100

    calender_Blue.py

--- Arts and Sports/calender_Blue.py
+++ Arts and Sports/calender_Blue.py
@@ -1,6 +1,6 @@
 import calendar
 import datetime
-import time
+#import time
 import re
 import shutil
 
@@ -15,11 +15,11 @@ bdayb = r'\b{current_day}\b'.format(current_day=current_day)
 bdayac = r"\033[34m\033[1m{current_day}\033[0m".format(current_day=current_day)
 
 # Replace the current day with blue colored text
-calendar_text = calendar.month(current_year, current_month)
-calendar_text = re.sub(bdayb, bdayac, calendar_text)
+cal_te = calendar.month(current_year, current_month)
+cal_te = re.sub(bdayb, bdayac, cal_te)
 
 #output to the screen
-terminal_size = shutil.get_terminal_size((80, 24))
-centered_text = calendar_text.rjust(terminal_size[1])
+#terminal_size = shutil.get_terminal_size((80, 24))
+#centered_text = calendar_text.rjust(terminal_size[1])
 
-print(centered_text)
+print(cal_te)
