23be8ae5800274868a755522fb6a04bd4eff53ff
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-25 21:17:31 +0100

    Password Generator

--- MIT_AI.ME/CyberSecurity/Secure Password Generator/Password_Generator.py
+++ MIT_AI.ME/CyberSecurity/Secure Password Generator/Password_Generator.py
@@ -1,8 +1,8 @@
 #While I strongly advocate for SSO(Single Sign On) with 2FA/MFA as part of a strong identity solution,
 #I thought creating a secure random password generator would be great, which is a better alternative to
-#reusing passwords on multiple platforms. Just run the code and sea..lol
-#CS-MENT-W (Chem/cyber-Sate-Magnet-Elect-Network-Transport-Wav) #killchain 7
-#shorts CS-TL (Chem/Cyber-Sate-Transport-Light) 4
+#reusing passwords on multiple platforms. Just run the code and sea or don't and space..lool
+#CS-MENT-W (Chemistry/Cybersecurity-Satellite-Magnetic-Electricity-Network-Transport-Wave) #killchain 7
+#shorts (CS-TL (Chemistry/Cybersecurity-Satellite-Transport-Light)) 4
 
 import secrets
 import string
