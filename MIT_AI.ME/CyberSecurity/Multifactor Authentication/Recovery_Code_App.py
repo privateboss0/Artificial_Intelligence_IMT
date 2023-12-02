1183f650aa5e49da4dce5e989d8b360058d0dbbb
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2023-12-02 17:49:12 +0100

    HOTP recovery code for 2FA/MFA app

--- MIT_AI.ME/CyberSecurity/Multifactor Authentication/Recovery_Code_App.py
+++ MIT_AI.ME/CyberSecurity/Multifactor Authentication/Recovery_Code_App.py
@@ -1,35 +1,43 @@
 import time
 import pyotp
 
-secretkey = "3Private6Public7" 
-#secretkey = pyotp.random_base32() Either use a static key or random key, both kept by the service provider.
-#but the program works inversely with the recovery code app secretkey set to static, while MFA and Integration app secretkey in random.
+secretkey = "4Private6Public7" 
 
 counter = 0
+max_attempt = 10
 
+#Recovery codes are typically emailed to the end user OOB(Out of Band)
 recovery_codes = pyotp.HOTP(secretkey)
 
 # This is the secret key for counter value of recovery codes
-print(recovery_codes.at(0))
-print(recovery_codes.at(1))
-print(recovery_codes.at(2))
-print(recovery_codes.at(3))
-print(recovery_codes.at(4))
-print(recovery_codes.at(5))
-print(recovery_codes.at(6))
-print(recovery_codes.at(7))
-print(recovery_codes.at(8))
-print(recovery_codes.at(9))
+(recovery_codes.at(0))
+(recovery_codes.at(1))
+(recovery_codes.at(2))
+(recovery_codes.at(3))
+(recovery_codes.at(4))
+(recovery_codes.at(5))
+(recovery_codes.at(6))
+(recovery_codes.at(7))
+(recovery_codes.at(8))
+(recovery_codes.at(9))
+(recovery_codes.at(10))
 
 while True:
-  code = input("Enter your Recovery Code please: ")
-  if recovery_codes.verify(code, counter):
-    counter = 0
-    print("You have sucessfully logged in with Recovery Codes!")
+    code = input("Enter your Recovery Code please: ")
 
-# Make the API call.
-# TODOLIST: Implement your API call here into your application.
-  break
+    if recovery_codes.verify(code, counter):
+        counter = 0
+        print("You have sucessfully logged in with Recovery Codes!")
 
-else:
-    print("Invalid recovery code. We advise to contact Support after 10 trials")
+        # Make the API call.
+        # TODOLIST: Implement your API call here into your application.
+
+        break
+
+    else:
+        counter += 1
+        print("Invalid recovery code. Please try again.")
+
+        if counter == max_attempt:
+            print("Invalid recovery code. We advise to contact Support after 10 trials")
+            break
