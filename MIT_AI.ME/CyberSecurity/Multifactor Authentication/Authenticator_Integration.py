d6defc071d54cfbde04700863bfcbbf5d1af8067
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2023-12-02 17:51:06 +0100

    QR Code download for authenticator integration

--- MIT_AI.ME/CyberSecurity/Multifactor Authentication/Authenticator_Integration.py
+++ MIT_AI.ME/CyberSecurity/Multifactor Authentication/Authenticator_Integration.py
@@ -2,12 +2,12 @@ import time
 import pyotp
 import qrcode
 
-secretkey = "3Private6Public7" 
+secretkey = "4Private6Public7" 
 #secretkey = pyotp.random_base32() Either use a static key or random key, both kept by the service provider.
 #but the program works inversely with the recovery code app secretkey set to static, while MFA and Integration app secretkey in random.
 
 Uniform_Resource_Identifier = pyotp.totp.TOTP(secretkey).provisioning_uri(name="Aola", issuer_name= "Privateboss_App")
-# Input an authentication service provider/oauth uri. I used an abituary app for testing purposes.
+# Input an authentication service provider/oauth uri. I used privateboss app for testing purposes
 # https://github.com/google/google-authenticator/wiki/Key-Uri-Format
 
 print(Uniform_Resource_Identifier)
