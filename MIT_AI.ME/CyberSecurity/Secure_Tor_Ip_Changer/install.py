93558607a5d7047dae61b2e90ede68eb87ddd52f
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2024-06-01 11:49:55 +0000

    install.py

--- MIT_AI.ME/CyberSecurity/Secure_Tor_Ip_Changer/install.py
+++ MIT_AI.ME/CyberSecurity/Secure_Tor_Ip_Changer/install.py
@@ -1,18 +1,21 @@
 import os
 choice = input('[+] to install press (Y) to uninstall press (N) >> ')
 run = os.system
+
 if str(choice) =='Y' or str(choice)=='y':
 
-    run('chmod 777 secureautoTOR.py')
-    run('mkdir /usr/share/aut')
-    run('cp secureautoTOR.py /usr/share/aut/secureautoTOR.py')
+    run('chmod 700 secureautoTOR.py')
+    run('mkdir /usr/share/ifa')
+    run('cp secureautoTOR.py /usr/share/ifa/secureautoTOR.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/secureautoTOR.py "$@"')
-    with open('/usr/bin/aut','w')as file:
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/ifa/secureautoTOR.py "$@"')
+    with open('/usr/bin/ifa','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/secureautoTOR.py')
-    print('''\n\ncongratulation Secure Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
+    run('chmod +x /usr/bin/ifa & chmod +x /usr/share/ifa/secureautoTOR.py')
+    print('''\n\nCongratulations!. Secure Tor Ip Changer has been sucessfully installed ''')
+    print("\nTo start the application type: ***sudo ifa*** in terminal ")
+    
 if str(choice)=='N' or str(choice)=='n':
-    run('rm -r /usr/share/aut ')
-    run('rm /usr/bin/aut ')
-    print('[!] now Secure Tor Ip Changer  has been removed successfully')
+    run('rm -r /usr/share/ifa ')
+    run('rm /usr/bin/ifa ')
+    print('[!] Secure Tor Ip Changer has been uninstalled successfully')
